b5de8499e2d6ed4d2c3bc970424bcff35321e61d
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2020-12-25 20:52:22 +0400

    test

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-function promise1() {
-  return new Promise(r => {
-    setTimeout(() => {
-      r('promise1')
-    }, 3000)
-  })
-}
+const { Transform } = require('stream');
+
+const myTransform = new Transform({
+    // writableObjectMode: true,
+
+  transform(chunk, encoding, callback) {
+    // Coerce the chunk to a number if necessary.
 
 
-function promise2() {
-  return new Promise(r => {
-    setTimeout(() => {
-      r('promise2')
-    }, 2000)
-  })
-}
+      // Transform the chunk into something else.
+    const data = chunk.toString().toUpperCase();
 
-Promise.coroutine(function * () {
+    // Push the data onto the readable queue.
+    callback(null, data);
+  }
+});
 
-  console.log('start');
+process.stdin.pipe(myTransform).pipe(process.stdout)
 
-  const v1 = yield promise1();
-
-  console.log('mid', v);
-
-  const v2 = yield promise2();
-
-  console.log('end', v2);
-  
-})
